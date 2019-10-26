be210e4fbdc06f6b00207417242f515d507abf8c
Scott <Scotthall1983@hotmail.com>
2019-10-26 11:30:57 +0100

    Chained a .then method to the end of onSearchSubmit function. Gave a name of response and set that to an arrow function. then colsole logged out the response.data.results to ensure that images are being recieved from the API.

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,8 @@ class App extends React.Component {
         Authorization:
           "Client-ID fd1ee3c285f1d6383333cb6901c5f7a41fa0cacb6a82043c9dd4e8553e3492c0 "
       }
+    }).then((response) => {
+      console.log(response.data.results)
     });
   }
 
