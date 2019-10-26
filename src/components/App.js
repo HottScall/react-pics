e1d151e9927431b977713d4d7c7e5fa926ef4b4c
Scott <Scotthall1983@hotmail.com>
2019-10-26 11:37:27 +0100

    Removed the .then function and turned onSearchSubmit into an an async function. then added const = response = await and chained that to the axios.get request.

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,15 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  async onSearchSubmit(term) {
+  const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization:
           "Client-ID fd1ee3c285f1d6383333cb6901c5f7a41fa0cacb6a82043c9dd4e8553e3492c0 "
       }
-    }).then((response) => {
-      console.log(response.data.results)
     });
+    console.log(response.data.results)
   }
 
   render() {
