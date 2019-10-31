e4dc9145cad4c5b9729771f922ae1208b218c31c
Scott <Scotthall1983@hotmail.com>
2019-10-31 09:24:34 +0000

    App.js - Imported the unsplash file and removed the call for Axios. Removed Axios.get call in side the onSS function and replaced it with unsplash.get. Check the change to the link in Unsplash and App.js. Content still renders to DOM.

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://api.unsplash.com/search/photos",
+  baseURL: "https://api.unsplash.com",
   headers: {
     Authorization:
       "Client-ID fd1ee3c285f1d6383333cb6901c5f7a41fa0cacb6a82043c9dd4e8553e3492c0 "
