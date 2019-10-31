0d763e1b775c6b88b98463f95f9f1881bc4d882e
Scott <Scotthall1983@hotmail.com>
2019-10-31 09:08:41 +0000

    Created state with a name of images set to an empty array, called setState to response.data.results.Initially just looking to return a number of images collected. Under the SearchBar component call, added this.state.images.length. Error recieved, setState is not a function.

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,25 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
+  state = { images: [] };
+
   async onSearchSubmit(term) {
-  const response = await axios.get("https://api.unsplash.com/search/photos", {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization:
           "Client-ID fd1ee3c285f1d6383333cb6901c5f7a41fa0cacb6a82043c9dd4e8553e3492c0 "
       }
     });
-    console.log(response.data.results)
+    console.log(this);
+    this.setState({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
