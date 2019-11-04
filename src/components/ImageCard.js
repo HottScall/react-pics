4acbbccea9f737789fd9cdee258e4b777dd2f741
Scott <Scotthall1983@hotmail.com>
2019-11-04 10:33:54 +0000

    Minor code refactor for better organisation of code. No additional code added to any files.

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,15 +2,11 @@ import React from "react";
 
 class ImageCard extends React.Component {
   render() {
-
-    const {description, urls} = this.props.image;
+    const { description, urls } = this.props.image;
 
     return (
       <div>
-        <img
-          alt={description}
-          src={urls.regular}
-        />
+        <img alt={description} src={urls.regular} />
       </div>
     );
   }
