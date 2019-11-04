b2fd992ba198d3e877e5668a9c4bc8022dac896b
Scott <Scotthall1983@hotmail.com>
2019-11-04 10:58:59 +0000

    Created a componentDidMount function and added an EventListener with call back of setSpans. Created a setSpans function and consolelogged out the imageRef.current.currentHeight

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 class ImageCard extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.imageRef = React.createRef();
+  }
+
+  componentDidMount(){
+    this.imageRef.current.addEventListener('load', this.setSpans)
+  }
+
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight)
   }
 
   render() {
