2226edf2182ced3b7182523ba7b04a6448c3a789
Scott <Scotthall1983@hotmail.com>
2019-11-05 11:18:24 +0000

    Changed the grid-auto-views to 10px and then changed the grid-gap to 0 10px, so no spaces on the vertical display of photos. Changed the Math.ceil from 200 to 10 to represent the change in css. DOM rendering content nicely.

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -14,8 +14,8 @@ class ImageCard extends React.Component {
 
   setSpans = () => {
     const height = this.imageRef.current.clientHeight
-    
-    const spans = Math.ceil(height / 200);
+
+    const spans = Math.ceil(height / 10);
 
     this.setState({ spans });
   };
